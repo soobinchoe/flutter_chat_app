fdb0665d9600453b6f1f9c3e8cd93aaf2efc21cc
soobinchoe <65845277+soobinchoe@users.noreply.github.com>
2024-01-22 14:05:25 +0900

    Creating Flutter Chat App

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            //welcome back message
+
+            // email textfield
+
+            // pw textfields
+
+            // login button
+
+            // register now
+          ],
+        ),
+      ),
+    );
+  }
+}

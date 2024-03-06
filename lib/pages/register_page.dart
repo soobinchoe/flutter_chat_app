1afd9adf1e1e66cf2a6ed0314ca59789476c80e1
soobinchoe <65845277+soobinchoe@users.noreply.github.com>
2024-03-06 20:07:35 +0800

    Create Register Page

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_chat_app/components/my_button.dart';
+import 'package:flutter_chat_app/components/my_textfield.dart';
+
+class RegisterPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  final TextEditingController _confirmPwController = TextEditingController();
+
+  RegisterPage({super.key});
+
+  // register method
+  void register() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            const SizedBox(
+              height: 50,
+            ),
+
+            //
+            Text(
+              "Let's create an account for you.",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            // email textfield
+            MyTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+
+            // pw textfields
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _pwController,
+            ),
+
+            const SizedBox(height: 10),
+
+            // confirm pw textfields
+            MyTextField(
+              hintText: "Confirm Password",
+              obscureText: true,
+              controller: _confirmPwController,
+            ),
+
+            const SizedBox(height: 25),
+
+            // login button
+            MyButton(
+              text: "Register",
+              onTap: register,
+            ),
+
+            const SizedBox(height: 25),
+
+            // register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already have an account? ",
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+                Text(
+                  "Login now",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
